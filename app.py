2ab60d3bd9ff3a508139ddf005221f1670692fcc
sathvikabramhani1 <sswayamp@gitam.in>
2026-03-27 00:00:25 +0530

    commit changes

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# Store last login attempts
+history = []
+
+def calculate_risk(data):
+    score = 0
+
+    if data.get("location") in ["Europe", "Russia", "Unknown", "Other"]:
+        score += 25
+
+    if data.get("device").lower() != "known":
+        score += 20
+
+    failed = int(data.get("failed_attempts", 0))
+    score += min(failed * 8, 30)
+
+    if data.get("time").lower() == "odd":
+        score += 25
+
+    return min(score, 100)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/evaluate", methods=["POST"])
+def evaluate():
+    data = request.json
+    score = calculate_risk(data)
+
+    if score < 30:
+        decision = "Allow"
+        color = "green"
+    elif score < 60:
+        decision = "MFA Required"
+        color = "orange"
+    else:
+        decision = "Block"
+        color = "red"
+
+    # Save history (last 5)
+    history.append({
+        "location": data.get("location"),
+        "score": score,
+        "decision": decision
+    })
+
+    if len(history) > 5:
+        history.pop(0)
+
+    return jsonify({
+        "risk_score": score,
+        "decision": decision,
+        "color": color,
+        "history": history
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
